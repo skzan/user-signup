f1e58629e7410f94600c8821148d045caed0f4cb
Shireen Korkzan <skzan15@sbcglobal.net>
2017-12-03 13:32:33 -0500

    Temporarily replaced import re with basic len function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 import cgi
 import os
 import jinja2
-import re
+#import re
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
@@ -12,8 +12,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-def is_invalid(text):
-    return re.search(r'\s+', text) or re.search(r'^.{0,2}$', text) or re.search(r'^\w{20}', text)
+# # def is_invalid(text):
+#     return re.search(r'\s+', text) or re.search(r'^.{0,2}$', text) or re.search(r'^\w{20}', text)
 
 
 @app.route("/", methods=['POST'])
@@ -27,32 +27,49 @@ def register():
     password_error = ""
     verify_error = ""
     email_error = ""
+    matching_error = ""
 
-    if is_invalid(username):
-        username = ""
+    if not len(username) < 3 and len(username) > 20:
         username_error = "That's not a valid username"
 
-    if is_invalid(password):
-        password = ""
+    if not len(password) <3 and len(password) >20:
         password_error = "That's not a valid password"
 
-    if verify == "":
-        verify_error = "Passwords don't match"
+    #if is_invalid(username):
+    #    username = ""
+        #username_error = "That's not a valid username"
 
-    if verify != password:
-        verify = ""
-        verify_error = "Passwords don't match"
+#    if is_invalid(password):
+#        password = ""
+#        password_error = "That's not a valid password"
+    if not len(verify) <3 and len(verify) >20:
+        verify_error = "That's not a valid password"
 
-    if email != "" or (email.count("@") != 1 or email.count(".") != 1 or is_invalid(email)):
+    if not verify == password:
+        matching_error = "Passwords don't match"
+
+    #if is_invalid(verify):
+    #    verify = ""
+    #    verify_error = "Passwords don't match"
+
+    #if verify != password:
+        #verify = ""
+        #verify_error = "Passwords don't match"
+
+    if email == "":
+        #or (email.count("@") != 1 or email.count(".") != 1:
         email_error = "That's not a valid email"
 
-    if username_error !="" and password_error !="" and verify_error !="" and email_error !="":
+    if email != email.count("@") != 1 or email.count(".") != 1:
+        email_error = "That's not a valid email"
+
+    if username_error !="" and password_error !="" and verify_error !="" and email_error !="" and matching_error !="":
         return redirect("/welcome?username=" + username)
     else:
         template = jinja_env.get_template("index.html")
         return render_template("index.html", username = username, email = email,
             username_error = username_error, password_error = password_error,
-            verify_error = verify_error, email_error = email_error)
+            verify_error = verify_error, email_error = email_error, matching_error = matching_error)
 
 
 @app.route("/")

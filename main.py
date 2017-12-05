bb852f192e67b0deda3018ac4a0ca6dc2ed5908c
Shireen Korkzan <skzan15@sbcglobal.net>
2017-12-04 19:16:16 -0500

    Replaced <title> with <body>

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 import os
-import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -15,7 +13,7 @@ def validate():
     password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
-
+    errors = False
     username_error = ""
     password_error = ""
     verify_error = ""
@@ -37,13 +35,9 @@ def validate():
     if email.count("@") != 1 or email.count(".") != 1:
         email_error = "That's not a valid email"
 
-    #if email != "":
-        #email = email
-
-    if not username_error: #=="": #and password_error =="" and verify_error =="" and email_error =="" and matching_error =="":
+    if not username_error:
         return render_template("welcome.html", username=username)
     else:
-        #template = jinja_env.get_template("index.html")
         return render_template("index.html", username = username, email = email, password = password, verify = verify,
             username_error = username_error, password_error = password_error,
             verify_error = verify_error, email_error = email_error, matching_error = matching_error)
@@ -51,7 +45,6 @@ def validate():
 
 @app.route("/")
 def index():
-    template = jinja_env.get_template("index.html")
     return render_template("index.html")
 
 app.run()
